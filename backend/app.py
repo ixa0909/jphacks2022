220dcb4ad4d40343ac6b16fb331278859c6f8012
ixa0909 <sdb01152@edu.osakafu-u.ac.jp>
2022-10-16 14:10:15 +0900

    sql 試してみた

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,32 @@
 # -*- coding: utf-8 -*-
 from flask import Flask,jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
 # 日本語を使えるように
 app.config['JSON_AS_ASCII'] = False
+app.config['MYSQL_USER'] = 'night'
+app.config['MYSQL_PASSWORD'] = '2k#TE,[PK'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_DB'] = 'test'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
 
 @app.route('/')
-def index():
-    data = {
-        "id": "tガバbヴァ馬",
-        "name": "test"
-    }
-    return jsonify(data)
+def CONNECT_DB():
+    CS = mysql.connection.cursor()
+    # CS.execute('''CREATE TABLE TABLE_NAME (id INTEGER, name VARCHAR(20))''')
+    CS.execute('''INSERT INTO TABLE_NAME VALUES (1, 'Harry')''')
+    CS.execute('''INSERT INTO TABLE_NAME VALUES (2, 'Arthor')''')
+    mysql.connection.commit()
 
+    CS.execute('''SELECT * FROM TABLE_NAME''')
+    Executed_DATA = CS.fetchall()
+    print(Executed_DATA)
+    return str(Executed_DATA[1]['name'])
 
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    app.run(host='127.0.0.1', port=8080)
