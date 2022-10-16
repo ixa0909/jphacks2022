ca737e413fd0c9022feb94470a8dbf91c0ce0ab3
ixa0909 <sdb01152@edu.osakafu-u.ac.jp>
2022-10-16 12:25:14 +0900

    json 返すようにした

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__)
 
+# 日本語を使えるように
+app.config['JSON_AS_ASCII'] = False
+
 @app.route('/')
 def index():
-    return 'Hello World'
+    data = {
+        "id": "tガバbヴァ馬",
+        "name": "test"
+    }
+    return jsonify(data)
 
 
 if __name__ == '__main__':
