4c03f5a892e9a5abb002cf1a0240dfce5e8b6cd6
ixa0909 <sdb01152@edu.osakafu-u.ac.jp>
2022-10-22 01:36:06 +0900

    dbアクセス認証コードを別ファイルに記入

--- backend/app.py
+++ backend/app.py
@@ -1,34 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from flask import *
-# from db_access import db_access
+from db_access import db_access
 
 app = Flask(__name__)
-
-# データベースへのアクセスを認証
-
-from dotenv import load_dotenv
-import os
-from flask_mysqldb import MySQL
-
-
-
-# データベースログインの PASSWORD を取得
-load_dotenv(override=True)
-PASSWORD = os.getenv('DATABASE_PASSWORD')
-MYSQL_USER = os.getenv('MYSQL_USER')
-MYSQL_HOST = os.getenv('MYSQL_HOST')
-MYSQL_DB = os.getenv('MYSQL_DB')
-
-# 日本語を使えるように
-app.config['JSON_AS_ASCII'] = False
-app.config['MYSQL_USER'] = MYSQL_USER
-app.config['MYSQL_PASSWORD'] = PASSWORD
-app.config['MYSQL_HOST'] = MYSQL_HOST
-app.config['MYSQL_DB'] = MYSQL_DB
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-
-mysql = MySQL(app)
+mysql = db_access(app)
 
 @app.route('/hello',methods=['GET'])
 def CONNECT():
